dddfd3b08138c58c63b8a02d30748fedd2caee9e
Jon <jon@netbook.(none)>
2012-04-23 18:50:34 +0100

    updated doc blocks in the content iterator class

--- classes/rpa/cms/iterator/content.php
+++ classes/rpa/cms/iterator/content.php
@@ -1,22 +1,44 @@
 <?php
 
-class Rpa_Cms_Iterator_Content implements RecursiveIterator, Countable
+class Rpa_Cms_Iterator_Content implements Iterator, Countable
 {
+	/**
+	 * @var 	array 	$_data 	The array of content data
+	 */
+	protected $_data = array();
 
-	$_data = array();
+	/**
+	 * @var 	string 	$_identifier 	The identifier of this collection of content (NULL == root iterator)
+	 */
+	protected $_identifier = NULL;
 
-	$_identifier_prefix = NULL;
+	/*
+	 * @var 	string 	$_path 	The path to the content file that cotains this content collection
+	 */
+	protected $_path = NULL;
 
-	$_path = NULL;
-
-	public function __construct(array $data, $path, $identifier_prefix = NULL)
+	/**
+	 * Intitalises the content iterator object and recursively inflates
+	 * the content data array by cycling through each node and instantiating
+	 * a content object of the correct type with the content data contained
+	 * within that node
+	 *
+	 * @param 	array 	$data 		The content data that describes the content objects to place within this iterator
+	 * @param 	string 	$path 		The path of the content contained within this iterator
+	 * @param 	string 	$identifier The identifier (within the content file) of this iterator
+	 * @return 	void
+	 */
+	public function __construct(array $data, $path, $identifier = NULL) 
 	{
 		$this->_data = $data;
 		$this->$_path = $path;
-		$this->_identifier_prefix = $identifier_prefix;
+		$this->_identifier = $identifier;
 		$this->inflate();
 	}
 
+	/*
+	 *
+	 */
 	protected function inflate()
 	{
 		foreach($this->_data as $identifier => &$item)
@@ -46,7 +68,7 @@ class Rpa_Cms_Iterator_Content implements RecursiveIterator, Countable
 			{
 				$locale = Arr::get($item, 'locale', Cms_Content::$locale);
 
-				$item = Arr::path(Cms_Content::$_loaded_content, $locale.'.'.$uri.'.'.$this->get_full_identifier($identifier);
+				$item = Arr::path(Cms_Content::$_loaded_content, $locale.'.'.$uri.'.'.$this->get_full_identifier($identifier));
 
 				if(!$item instanceOf Cms_Content)
 				{	
@@ -78,7 +100,7 @@ class Rpa_Cms_Iterator_Content implements RecursiveIterator, Countable
 
 		if($this->_identifier_prefix !== NULL)
 		{
-			$full_identifier = $this->_identifier_prefix.'.'.$full_identifier;
+			$full_identifier = $this->_identifier.'.'.$full_identifier;
 		}	
 
 		return $full_identifier;
